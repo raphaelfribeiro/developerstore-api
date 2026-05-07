12b55a7846710f2d1b46556de12429dee6db3db5
Raphael Ribeiro <5924116+raphaelfribeiro@users.noreply.github.com>
2026-05-07 15:07:39 -0300

    chore(infra): register new repositories in IoC and fix docker ports

--- template/backend/src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureModuleInitializer.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureModuleInitializer.cs
@@ -1,9 +1,8 @@
-﻿using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
 using Ambev.DeveloperEvaluation.ORM;
 using Ambev.DeveloperEvaluation.ORM.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ambev.DeveloperEvaluation.IoC.ModuleInitializers;
@@ -13,6 +12,17 @@ public class InfrastructureModuleInitializer : IModuleInitializer
     public void Initialize(WebApplicationBuilder builder)
     {
         builder.Services.AddScoped<DbContext>(provider => provider.GetRequiredService<DefaultContext>());
+
+        // User
         builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+        // Sale
+        builder.Services.AddScoped<ISaleRepository, SaleRepository>();
+
+        // Cart
+        builder.Services.AddScoped<ICartRepository, CartRepository>();
+
+        // Product
+        builder.Services.AddScoped<IProductRepository, ProductRepository>();
     }
 }
