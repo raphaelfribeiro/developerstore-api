6be81fc03316012db19cb39b93f2896cdb591085
Raphael Ribeiro <5924116+raphaelfribeiro@users.noreply.github.com>
2026-05-08 17:50:05 -0300

    fix(api): fix double envelope and missing mappings in AuthController

--- template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Auth/AuthenticateUserFeature/AuthenticateUserProfile.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Auth/AuthenticateUserFeature/AuthenticateUserProfile.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using Ambev.DeveloperEvaluation.Application.Auth.AuthenticateUser;
 using Ambev.DeveloperEvaluation.Domain.Entities;
 
 namespace Ambev.DeveloperEvaluation.WebApi.Features.Auth.AuthenticateUserFeature;
@@ -8,11 +9,12 @@ namespace Ambev.DeveloperEvaluation.WebApi.Features.Auth.AuthenticateUserFeature
 /// </summary>
 public sealed class AuthenticateUserProfile : Profile
 {
-    /// <summary>
-    /// Initializes a new instance of the <see cref="AuthenticateUserProfile"/> class
-    /// </summary>
     public AuthenticateUserProfile()
     {
+        CreateMap<AuthenticateUserRequest, AuthenticateUserCommand>();
+
+        CreateMap<AuthenticateUserResult, AuthenticateUserResponse>();
+
         CreateMap<User, AuthenticateUserResponse>()
             .ForMember(dest => dest.Token, opt => opt.Ignore())
             .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role.ToString()));
